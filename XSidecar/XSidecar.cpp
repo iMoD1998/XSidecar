9ce57b07d6f8a87d1a8af3dad31fb509920e874a
iMoD1998 <imod1998@gmail.com>
2019-05-04 17:47:12 +0100

    Added some more exports

--- XSidecar/XSidecar.cpp
+++ XSidecar/XSidecar.cpp
@@ -9,10 +9,15 @@ XSidecarEmulatorCloseKdPipe_t XSidecarEmulatorCloseKdPipe = NULL;
 XSidecarGetListItemCount_t XSidecarGetListItemCount = NULL;
 XSidecarOpenListItem_t XSidecarOpenListItem = NULL;
 XSidecarEmulatorSetPowerState_t XSidecarEmulatorSetPowerState = NULL;
+XSidecarEmulatorGetPowerState_t XSidecarEmulatorGetPowerState = NULL;
 XSidecarGetName_t XSidecarGetName = NULL;
 XSidecarGetConsoleSerialNumber_t XSidecarGetConsoleSerialNumber = NULL;
 XSidecarGetVersionInformation_t XSidecarGetVersionInformation = NULL;
 XSidecarCloseList_t XSidecarCloseList = NULL;
+XSidecarEmulatorSpiBegin_t XSidecarEmulatorSpiBegin = NULL;
+XSidecarEmulatorSpiEnd_t XSidecarEmulatorSpiEnd = NULL;
+XSidecarEmulatorSpiRead_t XSidecarEmulatorSpiRead = NULL;
+XSidecarEmulatorSpiWrite_t XSidecarEmulatorSpiWrite = NULL;
 
 bool XSidecarGetExports( HMODULE XSidecar )
 {
@@ -23,9 +28,14 @@ bool XSidecarGetExports( HMODULE XSidecar )
 	XSidecarGetListItemCount = (XSidecarGetListItemCount_t)GetProcAddress( XSidecar, "XSidecarGetListItemCount" );
 	XSidecarOpenListItem = (XSidecarOpenListItem_t)GetProcAddress( XSidecar, "XSidecarOpenListItem" );
 	XSidecarEmulatorSetPowerState = (XSidecarEmulatorSetPowerState_t)GetProcAddress( XSidecar, "XSidecarEmulatorSetPowerState" );
+	XSidecarEmulatorGetPowerState = (XSidecarEmulatorGetPowerState_t)GetProcAddress( XSidecar, "XSidecarEmulatorGetPowerState" );
 	XSidecarGetName = (XSidecarGetName_t)GetProcAddress( XSidecar, "XSidecarGetName" );
 	XSidecarGetConsoleSerialNumber = (XSidecarGetConsoleSerialNumber_t)GetProcAddress( XSidecar, "XSidecarGetConsoleSerialNumber" );
 	XSidecarGetVersionInformation = (XSidecarGetVersionInformation_t)GetProcAddress( XSidecar, "XSidecarGetVersionInformation" );
+	XSidecarEmulatorSpiBegin = (XSidecarEmulatorSpiBegin_t)GetProcAddress( XSidecar, "XSidecarEmulatorSpiBegin" );
+	XSidecarEmulatorSpiEnd = (XSidecarEmulatorSpiEnd_t)GetProcAddress( XSidecar, "XSidecarEmulatorSpiEnd" );
+	XSidecarEmulatorSpiRead = (XSidecarEmulatorSpiRead_t)GetProcAddress( XSidecar, "XSidecarEmulatorSpiRead" );
+	XSidecarEmulatorSpiWrite = (XSidecarEmulatorSpiWrite_t)GetProcAddress( XSidecar, "XSidecarEmulatorSpiWrite" );
 	return true;
 }
 
--- XSidecar/XSidecar.h
+++ XSidecar/XSidecar.h
@@ -2,17 +2,23 @@
 
 #include <Windows.h>
 
-typedef HANDLE( WINAPI * XSidecarOpenEmulatorList_t )					( );
-typedef HANDLE( WINAPI * XSidecarEmulatorOpenKdPipe_t )				( HANDLE Handle, LPCWSTR Name );
-typedef HANDLE( WINAPI * XSidecarEmulatorCloseKdPipe_t )				( HANDLE Pipe );
+typedef HANDLE	( WINAPI * XSidecarOpenEmulatorList_t )					( );
+typedef HANDLE	( WINAPI * XSidecarEmulatorOpenKdPipe_t )				( HANDLE Handle, LPCWSTR Name );
+typedef HANDLE	( WINAPI * XSidecarEmulatorCloseKdPipe_t )				( HANDLE Pipe );
 typedef void	( WINAPI * XSidecarCloseList_t )						( HANDLE List );
-typedef SIZE_T( WINAPI * XSidecarGetListItemCount_t )					( HANDLE List );
-typedef HANDLE( WINAPI * XSidecarOpenListItem_t )						( HANDLE List, int ListIndex );
-typedef HRESULT( WINAPI * XSidecarEmulatorSetPowerState_t )			( HANDLE Handle, BOOL Status );
-typedef HRESULT( WINAPI * XSidecarGetName_t )							( HANDLE Handle, LPWSTR Buffer, int Length, int Something );
-typedef HRESULT( WINAPI * XSidecarGetConsoleSerialNumber_t )			( HANDLE Handle, BYTE* Buffer );
-typedef HRESULT( WINAPI * XSidecarGetVersionInformation_t )			( HANDLE Handle, BYTE* Buffer );
-typedef HRESULT( WINAPI * XSidecarGetVersionInformation_t )			( HANDLE Handle, BYTE* Buffer );
+typedef SIZE_T	( WINAPI * XSidecarGetListItemCount_t )					( HANDLE List );
+typedef HANDLE	( WINAPI * XSidecarOpenListItem_t )						( HANDLE List, int ListIndex );
+typedef HRESULT	( WINAPI * XSidecarEmulatorSetPowerState_t )			( HANDLE Handle, BOOL Status );
+typedef HRESULT	( WINAPI * XSidecarEmulatorGetPowerState_t )			( HANDLE Handle, BOOL* Status );
+typedef HRESULT	( WINAPI * XSidecarGetName_t )							( HANDLE Handle, LPWSTR Buffer, int Length, int Something );
+typedef HRESULT	( WINAPI * XSidecarGetConsoleSerialNumber_t )			( HANDLE Handle, BYTE* Buffer );
+typedef HRESULT	( WINAPI * XSidecarGetVersionInformation_t )			( HANDLE Handle, BYTE* Buffer );
+typedef HRESULT	( WINAPI * XSidecarGetVersionInformation_t )			( HANDLE Handle, BYTE* Buffer );
+
+typedef HRESULT	( WINAPI * XSidecarEmulatorSpiBegin_t )					( HANDLE Handle );
+typedef HRESULT	( WINAPI * XSidecarEmulatorSpiEnd_t )					( HANDLE Handle );
+typedef HRESULT	( WINAPI * XSidecarEmulatorSpiRead_t )					( HANDLE Handle, BYTE* Buffer, SIZE_T Length, DWORD* BytesRead );
+typedef HRESULT	( WINAPI * XSidecarEmulatorSpiWrite_t )					( HANDLE Handle, BYTE* Buffer, SIZE_T Length);
 
 extern XSidecarOpenEmulatorList_t XSidecarOpenEmulatorList;
 extern XSidecarEmulatorOpenKdPipe_t XSidecarEmulatorOpenKdPipe;
@@ -21,9 +27,14 @@ extern XSidecarCloseList_t XSidecarCloseList;
 extern XSidecarGetListItemCount_t XSidecarGetListItemCount;
 extern XSidecarOpenListItem_t XSidecarOpenListItem;
 extern XSidecarEmulatorSetPowerState_t XSidecarEmulatorSetPowerState;
+extern XSidecarEmulatorGetPowerState_t XSidecarEmulatorGetPowerState;
 extern XSidecarGetName_t XSidecarGetName;
 extern XSidecarGetConsoleSerialNumber_t XSidecarGetConsoleSerialNumber;
 extern XSidecarGetVersionInformation_t XSidecarGetVersionInformation;
+extern XSidecarEmulatorSpiBegin_t XSidecarEmulatorSpiBegin;
+extern XSidecarEmulatorSpiEnd_t XSidecarEmulatorSpiEnd;
+extern XSidecarEmulatorSpiRead_t XSidecarEmulatorSpiRead;
+extern XSidecarEmulatorSpiWrite_t XSidecarEmulatorSpiWrite;
 
 bool LoadXSidecar( );
 
